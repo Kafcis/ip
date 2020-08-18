5510a6333825a45a8ac8b39700ee9366e3aa8b71
Marcus <kafcis.mc@gmail.com>
2020-08-19 04:35:54 +0800

    Level-0
    added Greet

---
+++ src/main/java/Greet.java
@@ -0,0 +1,5 @@
+public class Greet {
+    public static void main(String[] args) {
+        System.out.println("Salutations! My name is Duke\nHow may I help you today?\nNothing? That's surprising.");
+    }
+}
