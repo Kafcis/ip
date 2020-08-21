6cc1d52e0c51e970c4db78a23fe112348ea87b1b
Marcus <kafcis.mc@gmail.com>
2020-08-21 16:59:24 +0800

    Comit 0

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,5 +1,5 @@
 public class Greet {
     public static void main(String[] args) {
-        System.out.println("Salutations! My name is Duke\nHow may I help you today?\nNothing? That's surprising.");
+        System.out.println("Salutations! my name is Duke\nHow may I help you today?\nGoodbye");
     }
 }
