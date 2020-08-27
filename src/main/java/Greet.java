01e2c5afbed55be79a06764487a45f932fe8b7f6
Marcus <kafcis.mc@gmail.com>
2020-08-27 12:30:51 +0800

    Level 2 changed if else chain to switch

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -3,34 +3,44 @@ import java.util.*;
 public class Greet {
 
     private static String[] list= new String[100];
+    private static boolean[] checklist= new boolean[100];
     private static int listCounter= 0;
+    private static boolean isAwake=true;
 
     public static void main(String[] args) {
         System.out.println("Salutations! my name is Duke\nHow may I help you today?\n");
-        while (true) {
+        while (isAwake) {
             Scanner in = new Scanner(System.in);
             String command = in.nextLine();
-            if (command.equals("bye")) {
+            switch (command) {
+            case "bye": {
                 System.out.println("Goodbye");
+                isAwake=false;
                 break;
             }
-            else if (command.equals("list")){
-                for(int i=0; i<listCounter;i++){
-                    System.out.println(String.valueOf(i+1)+". "+list[i]);
+            case "list": {
+                for (int i = 0; i < listCounter; i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + list[i]);
                 }
+                break;
             }
-            else {
+            default: {
                 include(command);
                 System.out.println("Added: " + command);
             }
-
+            }
         }
     }
     public static void include(String args){
         list[listCounter]=args;
         listCounter++;
-
         }
+    public static void setCheck(int arg){
+        checklist[arg]=true;
+    }
+    public static boolean isCheck(int arg){
+        return checklist[arg];
+    }
 
 
 
