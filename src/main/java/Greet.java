893b2491e0956288d15a190d9f2e8f78e3823e2b
Marcus <kafcis.mc@gmail.com>
2020-08-27 11:14:06 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Greet {
+
+
     public static void main(String[] args) {
-        System.out.println("Salutations! my name is Duke\nHow may I help you today?\nGoodbye");
+        System.out.println("Salutations! my name is Duke\nHow may I help you today?\n");
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String command = in.nextLine();
+            if (command.equals("bye")){
+                System.out.println("Goodbye");
+                break;
+            }
+            else{
+                System.out.println("Echoing: "+command);
+
+            }
+        }
     }
+
+
+
+
 }
