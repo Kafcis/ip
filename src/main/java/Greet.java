834051fa2d503277ce1f153a6de56eac645cf968
Marcus <kafcis.mc@gmail.com>
2020-08-28 09:20:15 +0800

    Level 3. Mark as Done

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -11,35 +11,50 @@ public class Greet {
         System.out.println("Salutations! my name is Duke\nHow may I help you today?\n");
         while (isAwake) {
             Scanner in = new Scanner(System.in);
-            String command = in.nextLine();
-            switch (command) {
+            String input = in.nextLine();
+            String[] command = input.split(" ");
+            switch (command[0]) {
             case "bye": {
                 System.out.println("Goodbye");
                 isAwake=false;
                 break;
             }
             case "list": {
+                System.out.println("Here is the list of tasks");
                 for (int i = 0; i < listCounter; i++) {
-                    System.out.println(String.valueOf(i + 1) + ". " + list[i]);
+                    System.out.println(String.valueOf(i + 1) + "." + getCheck(i)  + " "+list[i]);
                 }
                 break;
             }
+            case "done":{
+                System.out.println("Nice! I've marked this task as done: ");
+                int value=Integer.parseInt(command[1]);
+                setCheck(value-1);
+                System.out.println(String.valueOf(value) + "." + getCheck(value-1)  + " "+list[value-1]);
+                break;
+            }
             default: {
-                include(command);
-                System.out.println("Added: " + command);
+                include(input);
+                System.out.println("Added: " + input);
             }
             }
         }
     }
     public static void include(String args){
         list[listCounter]=args;
+        checklist[listCounter]=false;
         listCounter++;
         }
     public static void setCheck(int arg){
         checklist[arg]=true;
     }
-    public static boolean isCheck(int arg){
-        return checklist[arg];
+    public static String getCheck(int arg){
+        if(checklist[arg]){
+            return "[✓]";
+        }
+        else{
+            return "[✗]";
+        }
     }
 
 
