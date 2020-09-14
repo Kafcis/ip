85817567ccb9d046e933f50d01b7b965587055e0
Marcus <kafcis.mc@gmail.com>
2020-09-14 11:41:53 +0800

    Level 6.Delete and A-Collections

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Greet {
     private static int MAX_TASK_SIZE = 100;
-    private static Task[] taskList = new Task[100];
+    private static ArrayList<Task> taskList = new ArrayList<Task>(100);
     private static int listCounter = 0;
     private static boolean isAwake =true;
     public static void main(String[] args) {
@@ -25,6 +27,10 @@ public class Greet {
                 doTask(command);
                 break;
             }
+            case "delete":{
+                doDelete(command);
+                break;
+            }
             default: {
                 includeTask(input);
             }
@@ -32,10 +38,23 @@ public class Greet {
         }
     }
 
+    private static void doDelete(String[] command) {
+        int value;
+        try {
+            value=Integer.parseInt(command[1]);
+        } catch(IndexOutOfBoundsException e) {
+            System.out.println("index required");
+            return;
+        }
+        System.out.println("Nice! I've removed this task : "+ taskList.get(value-1).toString());
+        taskList.remove(value-1);
+        listCounter--;
+    }
+
     private static void listTask() {
         System.out.println("Here is the list of tasks");
         for (int i = 0; i < listCounter; i++) {
-            System.out.println( i+1 + ". " + taskList[i].toString());
+            System.out.println( i+1 + ". " + taskList.get(i).toString());
         }
         System.out.println("there are "+ (listCounter) +" tasks in the list");
     }
@@ -48,8 +67,8 @@ public class Greet {
             System.out.println("index required");
             return;
         }
-        taskList[value-1].setDone(true);
-        System.out.println("Nice! I've marked this task as done: "+ taskList[value-1].toString());
+        taskList.get(value-1).setDone(true);
+        System.out.println("Nice! I've marked this task as done: "+ taskList.get(value-1).toString());
     }
 
     public static void includeTask(String args){
@@ -67,7 +86,7 @@ public class Greet {
                         " ____________________________________________________________\n");
                 return;
             }
-            taskList[listCounter] = new Deadline(description,args.substring(dividerPosition+4));
+            taskList.add(new Deadline(description,args.substring(dividerPosition+4)));
             break;
         }
         case "todo":{
@@ -81,7 +100,7 @@ public class Greet {
                 return;
             }
 
-            taskList[listCounter] = new Todo(description);
+            taskList.add(new Todo(description));
             break;
         }
         case "event":{
@@ -95,7 +114,7 @@ public class Greet {
                 return;
             }
 
-            taskList[listCounter] = new Event(description,args.substring(dividerPosition+4));
+            taskList.add(new Event(description,args.substring(dividerPosition+4)));
             break;
         }
         default: {
